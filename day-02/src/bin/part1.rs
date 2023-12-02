e68a65f88b1473a4e51c28b803c2db0310e5e794
RoopeK <35376295+Toopemus@users.noreply.github.com>
2023-12-02 23:10:16 +0200

    day 2 part 1

--- day-02/src/bin/part1.rs
+++ day-02/src/bin/part1.rs
@@ -1,9 +1,46 @@
+use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
 fn main() {
-    println!("Hello, World!");
+    let sum: i32 = sum_possible_games("games.txt");
+    println!("{}", sum);
+}
+
+fn get_possible_id(game: &str) -> Option<i32> {
+    let bag_contains = HashMap::from([
+        ("red", 12),
+        ("green", 13),
+        ("blue", 14),
+    ]);
+    let vec: Vec<&str> = game.split(':').collect();
+    let game_id = vec[0];
+    let game = vec[1];
+    let handfuls = game.split(|c| c == ',' || c == ';');
+    for handful in handfuls {
+        let vec: Vec<&str> = handful.trim().split(' ').collect();
+        let amount: i32 = vec[0].parse().unwrap();
+        let color = vec[1];
+        for (key, val) in &bag_contains {
+            if key == &color && val < &amount { return None }
+        }
+    }
+    let vec: Vec<&str> = game_id.split(' ').collect();
+    let id: i32 = vec[1].parse().unwrap();
+
+    Some(id)
+}
+
+fn sum_possible_games(filename: &str) -> i32 {
+    let mut sum: i32 = 0;
+    if let Ok(lines) = read_lines(filename) {
+        sum = lines
+            .filter_map(|line| line.ok())
+            .filter_map(|game| get_possible_id(&game))
+            .sum();
+    }
+    sum
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -19,7 +56,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn all_good() {
-        assert_eq!(1 + 2, 3);
+    fn get_possible_id_works() {
+        assert_eq!(get_possible_id("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green"), Some(1));
+        assert_eq!(get_possible_id("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue"), Some(2));
+        assert_eq!(get_possible_id("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red"), None);
+        assert_eq!(get_possible_id("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red"), None);
+        assert_eq!(get_possible_id("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"), Some(5));
     }
 }
